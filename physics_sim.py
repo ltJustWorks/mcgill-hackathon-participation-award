c1547873fecad38b20871623a0ba9bfb1216c453
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2020-11-07 15:21:27 -0500

    Update physics_sim.py

--- physics_sim.py
+++ physics_sim.py
@@ -5,6 +5,7 @@ height = 300
 
 screen = pygame.display.set_mode((width, height))
 screen.fill((255, 255, 255))
+pygame.draw.circle(screen, (255, 0, 0), (150, 150), 15)
 pygame.display.flip()
 
 running = True
